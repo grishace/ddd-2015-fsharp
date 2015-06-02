bc80918e42b7e83c8a99be6a457ed8378ac52e04
Grigoriy Belenkiy <grishace@gmail.com>
2015-06-01 20:30:03 -0600

    Adding code comments to the HelloWorld

--- code/01 - HelloWorld/Program.fs
+++ code/01 - HelloWorld/Program.fs
@@ -1,7 +1,14 @@
-﻿open System
+﻿// using namespace
+open System
 
+// entry point attribute for console app
 [<EntryPoint>]
+// main function
+// note: no args type
 let main argv =
   printfn "Hello World"
+  // ignore function result
+  // note: pipe operator - result from the left is passed as the last parameter to the next function
   Console.ReadLine() |> ignore
+  // return value, i.e. exit code
   0
