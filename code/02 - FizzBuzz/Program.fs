27ae01faf62ac0b35d4baff4a1e1703455469dd3
Grigoriy Belenkiy <grishace@gmail.com>
2015-06-01 23:13:02 -0600

    FizzBuzz implementation (no comments yet)

--- code/02 - FizzBuzz/Program.fs
+++ code/02 - FizzBuzz/Program.fs
@@ -1,5 +1,26 @@
 ﻿open System
 
+let fizz n =
+  if n % 3 = 0 
+    then (n, "Fizz")
+    else (n, sprintf "%d" n)
+
+let buzz ns =
+  match ns with
+  | (n, "Fizz") when n % 5 = 0 ->
+    (n, "FizzBuzz")
+  | (n, b) -> 
+    if n % 5 = 0 
+      then (n, "Buzz") 
+      else ns
+
+let fizzbuzz =
+  {1 .. 100} 
+  |> Seq.map fizz 
+  |> Seq.map buzz 
+  |> Seq.map snd
+
 [<EntryPoint>]
 let main argv = 
+  fizzbuzz |> Seq.iter (printfn "%s")
   0
