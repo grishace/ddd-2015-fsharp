dbbe95e278e6f9ba30b4407dca6fe7a91dd3ba73
Grigoriy Belenkiy <grishace@gmail.com>
2015-06-02 18:01:51 -0600

    Factorial - fold, reduce, recursion, and tail recursion

--- code/03 - Factorial/Program.fs
+++ code/03 - Factorial/Program.fs
@@ -1,4 +1,19 @@
-﻿[<EntryPoint>]
+﻿let fact n = {1 .. n} |> Seq.fold (*) 1
+
+let fact' n:bigint = {1I .. n} |> Seq.reduce (*)
+
+let rec f n =
+  match n with
+  | 0 | 1 -> 1
+  | _ -> n * f (n-1)
+
+let f' n =
+  let rec fr n a =
+    match n with
+    | 0 | 1 -> a
+    | _ -> fr (n - 1) (a * n)
+  fr n 1
+
+[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
     0
